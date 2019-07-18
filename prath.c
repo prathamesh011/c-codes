ae3e84001f28acbb141f5103a5edcc5a85e8e05e
prathamesh011 <53034133+prathamesh011@users.noreply.github.com>
2019-07-18 12:29:34 +0530

    Create prath.c

---
+++ prath.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+printf("hello prath");
+int num1,num2,num3,num4;
+printf("enter the num1:%d",num1);
+scanf("%d",&num1);
+
+printf("enter the num2:%d",num2);
+scanf("%d",&num2);
+
+
+printf("enter the num3:%d",num3);
+scanf("%d",&num3);
+
+
+printf("enter the num4:%d",num4);
+scanf("%d",&num4);
+
+sum=num1+num2;
+sub=num3-num4;
+printf("addition is:%d",sum);
+printf("substraction is:%d",sub);
+getch();
+}
+
+
+
+
+
