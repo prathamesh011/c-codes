39307381b7718a883c0bd716653bf10f6488a373
prathamesh011 <53034133+prathamesh011@users.noreply.github.com>
2019-07-18 12:34:47 +0530

    Update prath.c

--- prath.c
+++ prath.c
@@ -18,11 +18,12 @@ scanf("%d",&num3);
 
 printf("enter the num4:%d",num4);
 scanf("%d",&num4);
-
+mul=a*b*c*d;
 sum=num1+num2;
 sub=num3-num4;
 printf("addition is:%d",sum);
 printf("substraction is:%d",sub);
+printf("multiplication is:%d",mul);
 getch();
 }
 
